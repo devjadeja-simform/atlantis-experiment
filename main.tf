0c6f23327e8e8f4757bd9a98130f0d118362788b
Dev Jadeja <114152534+devjadeja-simform@users.noreply.github.com>
2024-03-21 14:41:23 +0530

    add

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "terraform-experimental-org"
+
+    workspaces {
+      name = "experimental"
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+
+variable "num_random_numbers" {
+  type    = number
+  default = 1
+}
+
+resource "random_integer" "example" {
+  count = var.num_random_numbers
+  keepers = {
+    first = "${timestamp()}"
+  }
+  min = 100000
+  max = 999999
+}
+
+output "one_time_passcode" {
+  value = random_integer.example[*].result
+}
