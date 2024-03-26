2156a3c12c01af2f60336704fbf7477e5323be6c
Dev Jadeja <114152534+devjadeja-simform@users.noreply.github.com>
2024-03-26 17:50:24 +0530

    change

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # atlantis-experiment
+
+1. Update `aws.credentials` in values.yaml before proceeding. This is necessary for atlantis to perform CRUD on state in S3 bucket.
+2. Create a bucket for storing state in it.
+3. Update the backend configuration with the S3 bucket name in `main.tf` file.
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "atlantisexp-tfstate-1"
+    bucket = "atlantisexp-tfstate"
     key    = "atlantis/experiment/terraform.tfstate"
     region = "us-east-1"
   }
@@ -18,7 +18,7 @@ terraform {
 
 variable "entity_count" {
   type    = number
-  default = 5
+  default = 2
 }
 
 resource "random_integer" "one_time_passcode" {
